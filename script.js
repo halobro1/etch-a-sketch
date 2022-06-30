f3b115e124d9866c536b0e278dc63ef24f4d1164
songying <gsy1911a1@gmail.com>
2022-06-30 22:36:52 +0800

    git commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function createGrid () {
+    const grid = document.getElementById('container')
+    const row = document.createElement('row')
+
+    grid.appendChild(row)
+
+    for (let i = 0; i < 17; i++){
+        
+    }
+    const square = document.createElement('square')
+}

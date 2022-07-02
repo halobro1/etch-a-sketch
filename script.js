e7a8c6b11fc5872b9cbde1c53b66c80d1b280d36
songying <gsy1911a1@gmail.com>
2022-07-02 17:54:12 +0800

    git commit

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 function populateBoard(size) {
     //creating 16 rows and 16 columns
     let board = document.querySelector(".board");
+    let squares = board.querySelectorAll('div') 
+    squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    let amount = size * size;
+
     //creating and repeating div 256(16*16) times 
-    for(let i = 0; i<256; i++) {
-        let square = document.createElement('div')
-        square.style.backgroundColor = 'blue'
-        board.insertAdjacentElement("beforeend", square)
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+         })
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement("beforeend", square);
     }
 }
 
 populateBoard(16);
 
 function changeSize(input) {
-    populateBoard(input)
+    if(input >=2 || input<=100){
+        populateBoard(input);
+    } else {
+        console.log("too many squares");
+    }
 }

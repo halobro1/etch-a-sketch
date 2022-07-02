409fc23e3f5dfd4ac3f0fc4d0820a77d2a076898
songying <gsy1911a1@gmail.com>
2022-07-02 16:14:11 +0800

    git commit git push

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-//creating 16 rows and 16 columns
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function populateBoard(size) {
+    //creating 16 rows and 16 columns
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-//
-for(let i = 0; i<256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue'
-    board.insertAdjacentElement("beforeend", square)
+    //creating and repeating div 256(16*16) times 
+    for(let i = 0; i<256; i++) {
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'blue'
+        board.insertAdjacentElement("beforeend", square)
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    populateBoard(input)
 }
